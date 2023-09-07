537fb10edff943978033c037bc3a6914a136c752
bomlang <bomlang4211@gmail.com>
2023-09-08 01:35:23 +0900

    Setting : react4조 사용될 라이브러리 설치 및 prettier,eslint 환경구성

---
+++ .prettierrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // ESLint와 통합 설정
+  eslintIntegration: true,
+
+  // 문자열을 작은 따옴표(')로 표시
+  singleQuote: true,
+
+  // 문장의 끝에 세미콜론(;)을 추가
+  semi: true,
+
+  // 탭(tab) 문자 대신 공백(space) 문자를 사용
+  useTabs: false,
+
+  // 들여쓰기 간격 (스페이스 2개 사용)
+  tabWidth: 2,
+
+  // 후행 쉼표(Trailing Comma) 설정 (객체와 배열 리터럴에 쉼표 추가)
+  trailingComma: 'es5',
+
+  // 한 줄의 최대 길이 (80자로 제한)
+  printWidth: 80,
+
+  // 화살표 함수 파라미터 항상 괄호로 감싸기
+  arrowParens: 'always',
+};
