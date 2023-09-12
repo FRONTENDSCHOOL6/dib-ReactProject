850b0f0488d887d1c57cf7ea43ede7e1e3604c90
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-13 08:36:31 +0900

    Fix : delay시간 4500으로 변경

--- src/components/main/MainBanner.jsx
+++ src/components/main/MainBanner.jsx
@@ -15,7 +15,7 @@ return (
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-        delay: 5000,
+        delay: 4500,
         disableOnInteraction: false,
         pauseOnMouseEnter: true,
         }}
