5e25c37e87d2e61413aca4d16fe12b659284d097
jjang-aaa <jadoit473@gmail.com>
2023-09-12 20:22:27 +0900

    Feat: login페이지 컴포넌트 조합

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,15 @@
+import LoginForm from '@/components/login/LoginForm';
+import LoginTitle from '@/components/login/LoginTitle';
+import SignUp from '@/components/login/SignUp';
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <LoginTitle />
+      <LoginForm />
+      <SignUp />
+    </>
+  );
 }
 
 export default Login;
