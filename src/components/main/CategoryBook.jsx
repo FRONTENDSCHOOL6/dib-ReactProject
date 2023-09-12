59f0a9ee43c886c8af133261c7b1bde3070c8775
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-13 08:23:25 +0900

    Feat : Main 페이지 main 부분 미완성

---
+++ src/components/main/CategoryBook.jsx
@@ -0,0 +1,15 @@
+
+function CategoryBook() {
+  return (
+    <>
+    <section className="bg-dibCategoryBg text-center">
+    <h2 className="text-dibBlack text-[70px] not-italic font-normal leading-[normal] tracking-[-1.5px]">
+      카테고리별 도서</h2>
+    <strong className="text-dibBlack text-[28px] not-italic font-normal">
+      카테고리별 도서에서 원하는 키워드로 도서를 찾아보세요!</strong>
+    </section>
+    </>
+  )
+}
+
+export default CategoryBook
