bea0e2a0d919fa767bb0c8ae5b248fe5e574fc01
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-13 08:20:39 +0900

    Feat : MainBanner Swiper 작용

---
+++ src/components/main/MainBanner.jsx
@@ -0,0 +1,50 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { useRef } from 'react';
+function MainBanner() {
+    const swiperRef = useRef(null)
+    
+return (
+    <>
+
+    <Swiper
+        ref={swiperRef}
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+        delay: 5000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+        }}
+        pagination={{
+        clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+    >
+        
+        <SwiperSlide>
+        <img
+            src="/MainBanner1.png"
+            alt="더 많은 개발 지식을 알고싶다
+            dib 추천도서"
+        />
+        </SwiperSlide>
+        <SwiperSlide>
+        <img
+            src="/MainBanner2.png"
+            alt="개발자라면?
+            개발자 필독도서는 못참지!"
+        />
+        </SwiperSlide>
+    </Swiper>
+
+    </>
+    );
+}
+
+export default MainBanner;
