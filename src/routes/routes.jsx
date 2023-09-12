bc0e0b0023b473a8dd23dda937c7415c32b39f0d
jjang-aaa <jadoit473@gmail.com>
2023-09-12 20:14:04 +0900

    Feat: routes.jsx에 Login 페이지 연결

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,17 +1,19 @@
-import Layout from '@/layout/Layout.jsx'
+import Layout from '@/layout/Layout.jsx';
 import { createBrowserRouter } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { createRoutesFromElements } from 'react-router-dom';
 import Home from '@/pages/Home';
 import WritePage from '@/pages/WirtePage';
+import Login from '@/pages/Login';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
-      <Route path='writePage' element={<WritePage />} />
-    </Route>,
-  ),
+      <Route path="writePage" element={<WritePage />} />
+      <Route path="login" element={<Login />} />
+    </Route>
+  )
 );
 
 export default router;
