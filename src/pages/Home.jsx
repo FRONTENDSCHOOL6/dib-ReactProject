713261110b3c5bc861f9f98e5e90d1df003b06ec
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-13 08:44:21 +0900

    Fix : Header 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 
+import HeaderBar from "@/layout/HeaderBar";
 import MainBanner from "@/components/main/MainBanner";
 import BestBook from "@/components/main/BestBook";
 import CategoryBook from "@/components/main/CategoryBook";
@@ -8,6 +9,7 @@ import FooterBar from "@/layout/FooterBar";
 function Home() {
   return (
     <>
+      <HeaderBar />
       <MainBanner />
       <BestBook />
       <CategoryBook />
