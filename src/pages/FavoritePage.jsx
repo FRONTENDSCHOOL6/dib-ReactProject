f27cdded1cd25b3b2bb7dc44ae4f39c98e5ada55
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-13 08:36:54 +0900

    FIx : Header 삭제

--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,10 +1,10 @@
 import FooterBar from "@/layout/FooterBar";
-import HeaderBar from "@/layout/HeaderBar";
+
 
 function Favorite() {
     return (
         <>
-            <HeaderBar />
+
             
             <FooterBar />
         </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import HeaderBar from "@/layout/HeaderBar";
+
 import MainBanner from "@/components/main/MainBanner";
 import BestBook from "@/components/main/BestBook";
 import CategoryBook from "@/components/main/CategoryBook";
@@ -8,7 +8,6 @@ import FooterBar from "@/layout/FooterBar";
 function Home() {
   return (
     <>
-      <HeaderBar />
       <MainBanner />
       <BestBook />
       <CategoryBook />
