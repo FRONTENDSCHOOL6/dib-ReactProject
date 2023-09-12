36f509a1367f7b3f7dc6d87278f835358b17f0ff
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-13 08:21:18 +0900

    Feat : Main 페이지 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
+import HeaderBar from "@/layout/HeaderBar";
+import MainBanner from "@/components/main/MainBanner";
+import BestBook from "@/components/main/BestBook";
+import CategoryBook from "@/components/main/CategoryBook";
+import NewBook from "@/components/main/NewBook";
+import FooterBar from "@/layout/FooterBar";
+
 function Home() {
-    return(
-        <>
-          <div>제발 되기를</div>
-        </>
-    )
+  return (
+    <>
+      <HeaderBar />
+      <MainBanner />
+      <BestBook />
+      <CategoryBook />
+      <NewBook />
+      <FooterBar />
+    </>
+  );
 }
 
 export default Home;
