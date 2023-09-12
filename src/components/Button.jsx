ae5841a2d5f30f953e5b274b3071ab6f10e6ef95
yena <spffhd0835@gmail.com>
2023-09-12 20:32:26 +0900

    Feat : 버튼 컴포넌트 추가 페이지에 재활용할 버튼 컴포넌트 추가

---
+++ src/components/Button.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+
+function Button({type,title,width="w-full",height="leading-[60px]"}) {
+  return (
+    <button
+      type={type}
+      className={`border rounded-full bg-primary text-white pl-5 ${height} text-lg mt-1 text-center ml-auto mr-auto ${width}`}
+    >
+      {title}
+    </button>
+  )
+}
+
+export default Button;
+
+Button.propTypes = {
+  type: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
+}
+
+
