18ad3b242300e25256b63a624587af3b2f9e0ea9
jjang-aaa <jadoit473@gmail.com>
2023-09-12 21:10:37 +0900

    Feat: 테일윈드css로 글자 위치, 사이즈 추가

--- src/components/login/SignUp.jsx
+++ src/components/login/SignUp.jsx
@@ -1,5 +1,9 @@
 function SignUp() {
-  return <a>회원가입</a>;
+  return (
+    <div className="flex flex-col gap-[30px] w-[600px] m-auto">
+      <a className="text-right mr-5 mt-2 text-lg">회원가입</a>
+    </div>
+  );
 }
 
 export default SignUp;
