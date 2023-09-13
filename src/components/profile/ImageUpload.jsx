37e25b80b7b933f305d1df04ff3d26cc13e6b6ef
jjang-aaa <jadoit473@gmail.com>
2023-09-13 19:28:13 +0900

    Feat: 프로필 사진 등록 input 코드작성 및 사진등록부분에 꾸밀 이미지 추가

--- src/components/profile/ImageUpload.jsx
+++ src/components/profile/ImageUpload.jsx
@@ -1,5 +1,16 @@
-function ImageUpload() {
-  return <div>ImageUpload</div>;
+function ImageUpload(selectedImage) {
+  return (
+    <div className="flex items-center justify-center space-x-4">
+      <label htmlFor="profileImage" className="sr-only">
+        프로필사진 등록하기
+      </label>
+      <input
+        type="file"
+        name="profile"
+        id="profileImage"
+        className="w-[150px] h-[150px] bg-blue-200"
+      />
+    </div>
+  );
 }
-
 export default ImageUpload;
