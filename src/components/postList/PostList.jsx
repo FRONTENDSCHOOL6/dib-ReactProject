d6b83dfe95e03c85084e06ab9ad7ed66dbeb898e
jjang-aaa <jadoit473@gmail.com>
2023-09-13 22:12:56 +0900

    Feat: 게시글 목록 페이지의 작성한 글 목록들 추가

---
+++ src/components/postList/PostList.jsx
@@ -0,0 +1,14 @@
+function PostList() {
+  return (
+    <div className="w-[1660px] m-auto">
+      <ul className="flex flex-wrap gap-10 justify-start mt-20 mb-20">
+        <li className="w-[380px] h-[584px] bg-slate-300">도서카드</li>
+        <li className="w-[380px] h-[584px] bg-slate-300">도서카드</li>
+        <li className="w-[380px] h-[584px] bg-slate-300">도서카드</li>
+        <li className="w-[380px] h-[584px] bg-slate-300">도서카드</li>
+      </ul>
+    </div>
+  );
+}
+
+export default PostList;
