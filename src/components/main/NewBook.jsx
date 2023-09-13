c8a6948fafa92185e7134b7bc4b6405bc7e2e08d
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-13 18:02:14 +0900

    Feat : 스크롤 버튼 추가

--- src/components/main/NewBook.jsx
+++ src/components/main/NewBook.jsx
@@ -1,12 +1,15 @@
 
+import { Link } from 'react-router-dom';
+import ScrollButton from '../common/ScrollButton';
 function NewBook() {
 return (
     <>
     <section className="text-center">
-    <h2 className="text-dibBlack text-[70px] not-italic font-normal leading-[normal] tracking-[-1.5px]">
+    <h2 className="text-dibBlack text-[40px] not-italic font-normal leading-[normal] tracking-[-1.5px]">
     신규 도서</h2>
-    <strong className="text-dibBlack text-[28px] not-italic font-normal">
+    <strong className="text-dibBlack text-[16px] not-italic font-normal">
     새롭게 소개하는 도서를 여기서 만나보세요!</strong>
+    <ScrollButton />
     </section>
     </>
     )
