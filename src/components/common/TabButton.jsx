463d16a9921c172baf05786f750ff2a71614a800
yena <spffhd0835@gmail.com>
2023-09-13 16:52:39 +0900

    Feat : 탭버튼 컴포넌트 추가 도서목록과 메인 카테고리도서에 사용될 버튼 컴포넌트 추가

---
+++ src/components/common/TabButton.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+
+function TabButton({
+  category,
+  borderColor = 'border-infoCategory',
+  textColor = 'text-infoCategory',
+  bgColor = 'bg-transparent',
+}) {
+  return (
+    <button
+      id="tab-1"
+      type="button"
+      //롤속성대신 아리아속성으로 쓰면 된다는거져? 아항
+      aria-label="tab"
+      //해당버튼이 선택됐는지
+      aria-selected='true'
+      //해당 본문과 연결하여 연관관계가있다는것을 명시
+      aria-controls="tab-panel-1"
+      className={`
+        w-[270px] 
+        h-16 
+        rounded-full 
+        border
+        text-2xl
+        ${borderColor}
+        ${textColor}
+        ${bgColor}
+      `}
+    >
+      {category}
+    </button>
+  )
+}
+
+export default TabButton
+
+
+TabButton.propTypes = {
+  category: PropTypes.string.isRequired,
+  borderColor: PropTypes.string,
+  textColor: PropTypes.string,
+  bgColor: PropTypes.string,
+};
