3ef2c36f0c630f0806c043e485ffe1d5a58d26b0
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-13 18:01:45 +0900

    Feat : 즐겨찾기 구현

--- src/pages/FavoritePage.jsx
+++ src/pages/FavoritePage.jsx
@@ -1,10 +1,12 @@
-import FooterBar from '@/layout/FooterBar';
+import SubVisualBanner from "@/components/common/SubVisualBanner";
 
-function Favorite() {
-  return (
-    <>
-      <FooterBar />
-    </>
-  );
+function FavoritePage() {
+    return (
+
+    <div className="w-screen flex items-center flex-col">
+    <SubVisualBanner title={'즐겨 찾기'} />
+    </div>
+    
+    );
 }
-export default Favorite;
+export default FavoritePage;
