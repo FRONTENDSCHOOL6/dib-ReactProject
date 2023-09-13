d7af5a5b71d384a2a25c2a860ec7bc83ed2c9ce8
jjang-aaa <jadoit473@gmail.com>
2023-09-13 19:29:08 +0900

    Feat: SignPageTitle 컴포넌트를 사용해서 html,css 코드작성

--- src/components/profile/ProfileTitle.jsx
+++ src/components/profile/ProfileTitle.jsx
@@ -1,5 +1,12 @@
+import SignPageTitle from '../common/SignPageTitle';
+
 function ProfileTitle() {
-  return <div>ProfileTitle</div>;
+  return (
+    <SignPageTitle
+      title="프로필등록"
+      subTitle="사용하실 닉네임과 사진을 등록해주세요."
+    />
+  );
 }
 
 export default ProfileTitle;
