db88b9cd19cc4a1ef53d0133979bc1523739799e
yena <spffhd0835@gmail.com>
2023-09-13 16:53:22 +0900

    Feat : 도서목록페이지 추가, 버튼 모음 컴포넌트 분리

--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -1,3 +1,4 @@
+import TabButtonList from "@/components/bookList/TabButtonList"
 import SubVisualBanner from "@/components/write/SubVisualBanner"
 
 
@@ -6,7 +7,26 @@ function BookList() {
   return (
     <>
       <SubVisualBanner title="도서목록"/>
+      <TabButtonList/>
 
+      <div className="w-[1660px] m-auto">
+          <ul 
+            id="tab-panel-1"
+            //현재본문과 연결된 버튼이 어떤버튼인지 명시
+            aria-labelledby="tab-1"
+            className="flex flex-wrap gap-10 justify-start mt-20 mb-20">
+            <li className="w-[380px] h-[584px] bg-slate-300">도서카드</li>
+            <li className="w-[380px] h-[584px] bg-slate-300">도서카드</li>
+            <li className="w-[380px] h-[584px] bg-slate-300">도서카드</li>
+            <li className="w-[380px] h-[584px] bg-slate-300">도서카드</li>
+            <li className="w-[380px] h-[584px] bg-slate-300">도서카드</li>
+            <li className="w-[380px] h-[584px] bg-slate-300">도서카드</li>
+            <li className="w-[380px] h-[584px] bg-slate-300">도서카드</li>
+            <li className="w-[380px] h-[584px] bg-slate-300">도서카드</li>
+            <li className="w-[380px] h-[584px] bg-slate-300">도서카드</li>
+            <li className="w-[380px] h-[584px] bg-slate-300">도서카드</li>
+          </ul>
+      </div>
     </>
   )
 }
