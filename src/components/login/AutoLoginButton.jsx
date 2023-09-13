106fc248f71cb36bb643976e07861e2949b6c2ea
jjang-aaa <jadoit473@gmail.com>
2023-09-13 22:57:02 +0900

    Feat: 자동로그인 마크업, 스타일링 추가

---
+++ src/components/login/AutoLoginButton.jsx
@@ -0,0 +1,13 @@
+function AutoLoginButton() {
+  return (
+    <div className="flex flex-row items-center gap-1 w-[600px]">
+      <label htmlFor="autoLogin">
+        <img src="/checked.png" alt="" className="w-6 h-6" />
+      </label>
+      <input type="checkbox" id="autoLogin" className="ml-5 mt-2 hidden" />
+      <span className="text-lg">자동로그인</span>
+    </div>
+  );
+}
+
+export default AutoLoginButton;
