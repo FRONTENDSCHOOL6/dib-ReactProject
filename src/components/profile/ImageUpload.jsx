e64576cabb7b38a319eebde87fd9cb9cc2d88f66
jjang-aaa <jadoit473@gmail.com>
2023-09-16 01:46:16 +0900

    Feat: 사진 업로드 이미지 수정, 사진 업로드 부분 이미지로 대체함

--- src/components/profile/ImageUpload.jsx
+++ src/components/profile/ImageUpload.jsx
@@ -1,14 +1,14 @@
-function ImageUpload(selectedImage) {
+function ImageUpload() {
   return (
     <div className="flex items-center justify-center space-x-4">
-      <label htmlFor="profileImage" className="sr-only">
-        프로필사진 등록하기
+      <label htmlFor="profileImage" className="cursor-pointer">
+        <img src="/emptyProfile.png" alt="프로필사진 등록하기" />
       </label>
       <input
         type="file"
         name="profile"
         id="profileImage"
-        className="w-[150px] h-[150px] bg-blue-200"
+        className="w-[150px] h-[150px] hidden"
       />
     </div>
   );
