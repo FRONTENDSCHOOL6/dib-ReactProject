210894c2bef569b5dda8110ea0fdb899ff590437
jjang-aaa <jadoit473@gmail.com>
2023-09-16 01:36:42 +0900

    Fix: 버튼 너비 추가

--- src/components/profile/RegistrationButton.jsx
+++ src/components/profile/RegistrationButton.jsx
@@ -1,7 +1,7 @@
 import Button from '../common/Button';
 
 function RegistrationButton() {
-  return <Button type="submit" title="등록하기" />;
+  return <Button type="submit" title="등록하기" width="w-[500px]" />;
 }
 
 export default RegistrationButton;
