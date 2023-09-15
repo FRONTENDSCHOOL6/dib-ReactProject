d9a3e016173e0fad30ee4adb9d2f909e8c84968d
jjang-aaa <jadoit473@gmail.com>
2023-09-16 01:35:58 +0900

    Fix: 닉네임 입력칸 사이즈 추가, 안내문구 색상과 사이즈 수정

--- src/components/profile/NicknameInput.jsx
+++ src/components/profile/NicknameInput.jsx
@@ -2,7 +2,7 @@ import FormInput from '../common/FormInput';
 
 function NicknameInput() {
   return (
-    <div className="flex flex-col gap-2 text-left text-lg">
+    <div className="flex flex-col gap-2 text-left text-lg w-[500px]">
       <FormInput
         type="text"
         label="닉네임등록"
@@ -11,7 +11,9 @@ function NicknameInput() {
         id="nickName"
         placeholder="닉네임을 입력해주세요."
       />
-      <span className="text-[#FF0000]">닉네임은 몇자 이하로 입력해주세요.</span>
+      <span className="text-dibRed text-base">
+        닉네임은 몇자 이하로 입력해주세요.
+      </span>
     </div>
   );
 }
