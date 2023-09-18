628f9525e827bebef5f7435b96e83e3f7a97e1dd
jjang-aaa <jadoit473@gmail.com>
2023-09-19 01:54:08 +0900

    Feat: 닉네임 유효성 검사 추가

--- src/components/profile/NicknameInput.jsx
+++ src/components/profile/NicknameInput.jsx
@@ -1,6 +1,22 @@
+import { useState } from 'react';
 import FormInput from '../common/FormInput';
 
 function NicknameInput() {
+  const [nickname, setNickname] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleNicknameOnChange = (event) => {
+    const newNickname = event.target.value;
+    setNickname(newNickname);
+
+    const regex = /^[가-힣]{1,8}$/;
+    if (!regex.test(newNickname)) {
+      setErrorMessage('한글 8자 이하로 입력해주세요.');
+    } else {
+      setErrorMessage('');
+    }
+  };
+
   return (
     <div className="flex flex-col gap-2 text-left text-lg w-[500px]">
       <FormInput
@@ -10,10 +26,12 @@ function NicknameInput() {
         name="nickName"
         id="nickName"
         placeholder="닉네임을 입력해주세요."
+        value={nickname}
+        onChange={handleNicknameOnChange}
       />
-      <span className="text-dibRed text-base">
-        닉네임은 몇자 이하로 입력해주세요.
-      </span>
+      {errorMessage && (
+        <span className="text-dibRed text-base">{errorMessage}</span>
+      )}
     </div>
   );
 }
