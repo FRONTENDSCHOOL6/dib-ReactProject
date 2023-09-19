8b299a2bcb1a190c539ab117c9fea4d34ca294d0
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-20 06:23:53 +0900

    Fix : 베스트 도서 like_count 순으로 렌더링

--- src/components/main/BestBook.jsx
+++ src/components/main/BestBook.jsx
@@ -1,4 +1,4 @@
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 import { pb } from '@/api/pocketbase';
 import ColBookCard from '../common/bookCards/ColBookCard';
 
@@ -18,29 +18,33 @@ function BestBook() {
     fetchBestBooks();
   }, []);
 
-    return (
-      <>
-        <section className="text-center w-[1920px] h-[670px] m-auto flex flex-col mb-[60px]">
-          <h2 className="text-dibBlack text-[32px] not-italic font-normal leading-[normal] tracking-[-1.5px] mt-[60px] mb-[20px]">
-            베스트 도서
-          </h2>
-          <strong className="text-dibBlack text-[20px] not-italic font-normal">
-            dib에서 가장 인기있는 책을 소개합니다!!
-          </strong>
-          <div className="w-[1200px] mx-auto">
-            <ul className="flex justify-center gap-6 my-10">
+  return (
+    <>
+      <section className="text-center w-[1920px] h-[670px] m-auto flex flex-col mb-[60px]">
+        <h2 className="text-dibBlack text-[32px] not-italic font-normal leading-[normal] tracking-[-1.5px] mt-[60px] mb-[20px]">
+          베스트 도서
+        </h2>
+        <strong className="text-dibBlack text-[20px] not-italic font-normal">
+          dib에서 가장 인기있는 책을 소개합니다!!
+        </strong>
+        <div className="w-[1200px] mx-auto">
+          <ul className="flex justify-center gap-6 my-10">
             {data.map((item) => (
-                <li key={item.id}>
-                  {/* 이미지와 제목 출력 */}
-                  <ColBookCard data={item} />
-                </li>
-              ))}
-            </ul>
-          </div>
-        </section>
-      </>
-    );
-  }
-
+              <li key={item.id}>
+                <ColBookCard
+                  imgSrc={item.book_image_link}
+                  imgAlt={item.book_title}
+                  nickName={item.user_id[0]}
+                  postTitle={item.post_title}
+                  bookTitle={item.book_title}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+}
 
 export default BestBook;
