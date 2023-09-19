ce40a50773990805c601dcd0f08c94d3f784dbf1
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-20 06:23:10 +0900

    Fix : ColBookCard 렌더링

--- src/components/common/bookCards/ColBookCard.jsx
+++ src/components/common/bookCards/ColBookCard.jsx
@@ -1,13 +1,26 @@
 import BookCardImage from './BookCardImage';
 import ColBookCardInfo from './ColBookCardInfo';
+import PropTypes from 'prop-types';
 
-function ColBookCard() {
+function ColBookCard({ postTitle, bookTitle, nickName, imgSrc, imgAlt }) {
   return (
     <div className="shadow-[5px_5px_10px_0px_rgba(0,0,0,0.25)]">
-      <BookCardImage />
-      <ColBookCardInfo />
+      <BookCardImage imgSrc={imgSrc} imgAlt={imgAlt} />
+      <ColBookCardInfo
+        nickName={nickName}
+        postTitle={postTitle}
+        bookTitle={bookTitle}
+      />
     </div>
   );
 }
+
+ColBookCard.propTypes = {
+  postTitle: PropTypes.string.isRequired,
+  nickName: PropTypes.string.isRequired,
+  bookTitle: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired,
+  imgAlt: PropTypes.string.isRequired,
+};
 
 export default ColBookCard;
