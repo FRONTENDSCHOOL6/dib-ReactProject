c49edde77f8eda8f493ad557e8cd9b95f92cfdfe
jjang-aaa <jadoit473@gmail.com>
2023-09-19 09:49:44 +0900

    Fix: 바텀 마진 삭제

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ import LoginTitle from '@/components/login/LoginTitle';
 
 function Login() {
   return (
-    <div className="flex flex-col mb-[100px]">
+    <div className="flex flex-col">
       <LoginTitle />
       <LoginForm />
       <JoinButton />
