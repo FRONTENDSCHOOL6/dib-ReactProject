9b658869d5777fc9e4f0cf3afa953738e3eaed2a
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-20 06:23:32 +0900

    Fix : RowBookCard 렌더링

--- src/components/common/bookCards/RowBookCard.jsx
+++ src/components/common/bookCards/RowBookCard.jsx
@@ -1,13 +1,26 @@
 import BookCardImage from './BookCardImage';
 import RowBookCardInfo from './RowBookCardInfo';
+import PropTypes from 'prop-types';
 
-function RowBookCard() {
+function RowBookCard({ postTitle, bookTitle, nickName, imgSrc, imgAlt }) {
   return (
     <div className="flex w-[560px] h-[284px] ">
-      <BookCardImage />
-      <RowBookCardInfo/>
+      <BookCardImage imgSrc={imgSrc} imgAlt={imgAlt} />
+      <RowBookCardInfo
+        nickName={nickName}
+        postTitle={postTitle}
+        bookTitle={bookTitle}
+      />
     </div>
   );
 }
+
+RowBookCard.propTypes = {
+  postTitle: PropTypes.string.isRequired,
+  nickName: PropTypes.string.isRequired,
+  bookTitle: PropTypes.string.isRequired,
+  imgSrc: PropTypes.string.isRequired,
+  imgAlt: PropTypes.string.isRequired,
+};
 
 export default RowBookCard;
--- src/components/common/bookCards/RowBookCardInfo.jsx
+++ src/components/common/bookCards/RowBookCardInfo.jsx
@@ -1,83 +1,53 @@
 import PropTypes from 'prop-types';
-import { pb } from '@/api/pocketbase';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
-function RowBookCardInfo({ postId, bookId }) {
-  const [data, setData] = useState([]);
+function RowBookCardInfo({ postTitle, bookTitle,nickName }) {
   const [liked, setLiked] = useState(false);
-
   const handleLikeClick = () => {
     setLiked(!liked);
     console.log(liked ? '좋아요 취소' : '좋아요 버튼이 클릭되었습니다.');
   };
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const postData = await pb.collection('posts').getFullList();
-        
-        setData(postData);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchData();
-  }, []);
+  return (
+    <div className="bg-white py-5 px-7 w-[280px] h-[284px] flex flex-col justify-center border-l-8 border-l-[#DCDCDC] border-dashed">
+      <h3 className="text-xl text-left font-semibold mb-[40px] truncate">
+        {postTitle}
+      </h3>
+      <h4 className="text-base text-left mb-[10px] truncate">{bookTitle}</h4>
 
-  if (data.length > 0) {
-    const filteredData = data.filter(
-      (item) => item.post_id === postId && item.book_id === bookId
-    );
-
-    if (filteredData.length > 0) {
-      const item = filteredData[0];
-
-      return (
-        <div className="bg-white py-5 px-7 w-[280px] h-[284px] flex flex-col justify-center border-l-8 border-l-[#DCDCDC] border-dashed">
-          <h3 className="text-xl text-left font-semibold mb-[40px] truncate">
-            {item.post_title}
-          </h3>
-          <h4 className="text-base text-left mb-[10px] truncate">
-            {item.book_title}
-          </h4>
-
-          <div className="flex items-center justify-between w-[217px]">
-            <div className="w-[200px] h-[28px] flex items-center mr-10">
-              <div className="w-7 h-7 rounded-full bg-slate-300 mr-[5px]" />
-              {/* 사용자 아이디 출력 */}
-              <span className="text-sm">{item.user_id}</span>
-            </div>
-            <div>
-              <form>
-                <input
-                  type="checkbox"
-                  onClick={() => handleLikeClick(item)}
-                  aria-pressed={liked}
-                  aria-label={liked ? '' : '좋아요 취소'}
-                  id={`heartButton-${postId}-${bookId}`}
-                  hidden
-                />
-                <label htmlFor={`heartButton-${postId}-${bookId}`}>
-                  {liked ? (
-                    <img src="/heart.png" alt="" />
-                  ) : (
-                    <img src="/emptyheart.png" alt="" />
-                  )}
-                </label>
-              </form>
-            </div>
-          </div>
+      <div className="flex items-center justify-between w-[217px]">
+        <div className="w-[200px] h-[28px] flex items-center mr-10">
+          <div className="w-7 h-7 rounded-full bg-slate-300 mr-[5px]"></div>
+          <span className="text-sm">{nickName}</span>
         </div>
-      );
-    }
-  }
-
-  return null;
+        <div>
+          <form>
+            <input
+              type="checkbox"
+              onClick={handleLikeClick}
+              aria-pressed={liked}
+              aria-label={liked ? '' : '좋아요 취소'}
+              id="heartButton1"
+              hidden
+            />
+            <label htmlFor="heartButton1">
+              {liked ? (
+                <img src="/heart.png" alt="" />
+              ) : (
+                <img src="/emptyheart.png" alt="" />
+              )}
+            </label>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-RowBookCardInfo.propTypes = {
-  postId: PropTypes.string.isRequired,
-  bookId: PropTypes.string.isRequired,
-};
-
 export default RowBookCardInfo;
+
+RowBookCardInfo.propTypes = {
+  postTitle: PropTypes.string.isRequired,
+  bookTitle: PropTypes.string.isRequired,
+  nickName: PropTypes.string.isRequired,
+};
