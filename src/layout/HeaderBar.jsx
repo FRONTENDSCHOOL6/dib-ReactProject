72fc6122fa61f6cf450ec425548e6c31b7d93ed5
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-20 15:35:40 +0900

    Fix : framer-motion 적용

--- src/layout/HeaderBar.jsx
+++ src/layout/HeaderBar.jsx
@@ -1,17 +1,41 @@
 import { Link } from 'react-router-dom';
-
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 function HeaderBar() {
+  const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+  const [prevScrollPos, setPrevScrollPos] = useState(window.pageYOffset);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      setIsHeaderVisible(
+        prevScrollPos > currentScrollPos || currentScrollPos < 100
+      );
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos]);
+
   return (
     <header
-      className="w-screen h-20 bg-white bg-opacity-50 border-b fixed top-0 left-0 z-[100] "
+      className={`w-screen h-20 bg-white bg-opacity-50 border-b fixed top-0 left-0 z-[100] ${
+        isHeaderVisible ? '' : 'transform -translate-y-full'
+      }`}
       style={{ backdropFilter: 'blur(10px)' }}
     >
       <div className="w-[1200px] h-20 m-auto flex justify-between items-center relative">
-        <h1>
+        <motion.h1
+            initial={{ opacity: 0,y:-600 }}
+            animate={{ opacity: 4,y:0 }}>
           <Link to="/">
             <img src="/logoBlack.png" alt="개발자의 도서리뷰 dib" />
           </Link>
-        </h1>
+        </motion.h1>
         <nav className="absolute left-52">
           <ul className="flex gap-24">
             <li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,15 @@ import MainBanner from '@/components/main/MainBanner';
 import BestBook from '@/components/main/BestBook';
 import CategoryBook from '@/components/main/CategoryBook';
 import NewBook from '@/components/main/NewBook';
-
+import { motion } from 'framer-motion';
 
 function Home() {
   return (
     <>
+      <motion.h1
+        initial={{ opacity: 0, y: -500 }}
+        animate={{ opacity: 1, y: 0 }}
+      ></motion.h1>
       <MainBanner />
       <BestBook />
       <CategoryBook />
