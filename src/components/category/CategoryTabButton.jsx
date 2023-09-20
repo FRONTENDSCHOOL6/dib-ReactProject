7a4ed889c0dcdcc9f7142cabc9981889111b4866
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-20 17:29:51 +0900

    Feat : 애니메이션 구현

--- src/components/category/CategoryTabButton.jsx
+++ src/components/category/CategoryTabButton.jsx
@@ -1,7 +1,13 @@
 import { useState } from 'react';
 import TabButton from '../common/TabButton';
+import PropTypes from 'prop-types';
 
-function CategoryTabButton() {
+function CategoryTabButtonList({
+  cssClick,
+  htmlClick,
+  reactClick,
+  javascriptClick,
+}) {
   const [activeCategory, setActiveCategory] = useState(null);
 
   const handleButtonClick = (category) => {
@@ -9,74 +15,87 @@ function CategoryTabButton() {
   };
 
   return (
-    <div className="w-[1920] m-[50px]">
+    <div className="w-[1920px] m-auto">
       <ul aria-label="tablist" className="inline-flex gap-10 items-start">
         <li>
           <TabButton
             category="HTML"
-            onClick={() => handleButtonClick('HTML')}
-            bgColor={activeCategory === 'HTML' ? 'bg-primary' : 'bg-transparent'}
-            textColor={activeCategory === 'HTML' ? 'text-white' : 'text-infoCategory'}
-            borderColor={activeCategory === 'HTML' ? 'border-transparent' : 'border-infoCategory'}
+            onClick={() => handleButtonClick(htmlClick)}
+            bgColor={
+              activeCategory === 'HTML' ? 'bg-primary' : 'bg-transparent'
+            }
+            textColor={
+              activeCategory === 'HTML' ? 'text-white' : 'text-infoCategory'
+            }
+            borderColor={
+              activeCategory === 'HTML'
+                ? 'border-transparent'
+                : 'border-infoCategory'
+            }
           />
         </li>
         <li>
           <TabButton
             category="CSS"
-            onClick={() => handleButtonClick('CSS')}
-            bgColor={activeCategory === 'CSS' ? 'bg-primary' : 'bg-transparent'}
-            textColor={activeCategory === 'CSS' ? 'text-white' :'text-infoCategory'}
-            borderColor={activeCategory === 'CSS'
-              ? "border-transparent"
-              : 'border-infoCategory'
-           }
-         />
-       </li>
-       <li>
-         <TabButton
-           category="Javascript"
-           onClick={() => handleButtonClick('Javascript')}
-           bgColor={
-             activeCategory === "Javascript" 
-               ? "bg-primary" 
-               : 'bg-transparent'
-           }
-           textColor={
-             activeCategory === "Javascript" 
-               ? "text-white" 
-               :'text-infoCategory'
-           }
-           borderColor={
-            activeCategory === "Javascript"
-               ? "border-transparent"
-               : 'border-infoCategory'
-           }
-         />
-       </li>
-       <li>
-         <TabButton
-           category="React"
-           onClick={() => handleButtonClick("React")}
-           bgColor={
-            activeCategory=== "React" 
-               ? "bg-primary" 
-               :'bg-transparent'
-          }
-          textColor={
-            activeCategory=== "React" 
-                ? "text-white" 
-                :'text-infoCategory'
-          }
-          borderColor={
-            activeCategory=== "React"
-                ? "border-transparent"
+            onClick={() => handleButtonClick(cssClick)}
+            bgColor={
+              activeCategory === 'CSS' ? 'bg-primary' : 'bg-transparent'}
+            textColor={
+              activeCategory === 'CSS' ? 'text-white' : 'text-infoCategory'
+            }
+            borderColor={
+              activeCategory === 'CSS'
+                ? 'border-transparent'
                 : 'border-infoCategory'
-          }
-        />
-      </li>
-    </ul>
-  </div>
-);
+            }
+          />
+        </li>
+        <li>
+          <TabButton
+            category="Javascript"
+            onClick={() => handleButtonClick(javascriptClick)}
+            bgColor={
+              activeCategory === javascriptClick ? 'bg-primary' : 'bg-transparent'
+            }
+            textColor={
+              activeCategory === javascriptClick
+                ? 'text-white'
+                : 'text-infoCategory'
+            }
+            borderColor={
+              activeCategory === javascriptClick
+                ? 'border-transparent'
+                : 'border-infoCategory'
+            }
+          />
+        </li>
+        <li>
+          <TabButton
+            category="React"
+            onClick={() => handleButtonClick(reactClick)}
+            bgColor={
+              activeCategory === 'React' ? 'bg-primary' : 'bg-transparent'
+            }
+            textColor={
+              activeCategory === 'React' ? 'text-white' : 'text-infoCategory'
+            }
+            borderColor={
+              activeCategory === 'React'
+                ? 'border-transparent'
+                : 'border-infoCategory'
+            }
+          />
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default CategoryTabButton;
+export default CategoryTabButtonList;
+
+CategoryTabButtonList.propTypes = {
+  reactClick: PropTypes.func.isRequired,
+  javascriptClick: PropTypes.func.isRequired,
+  cssClick: PropTypes.func.isRequired,
+  htmlClick: PropTypes.func.isRequired,
+};
--- src/components/main/CategoryBook.jsx
+++ src/components/main/CategoryBook.jsx
@@ -5,8 +5,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { useState, useEffect } from 'react';
 import { pb } from '@/api/pocketbase';
+
 function CategoryBook() {
   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
   useEffect(() => {
     pb.autoCancellation(false);
@@ -14,37 +16,45 @@ function CategoryBook() {
       const CategoryRecords = await pb.collection('posts').getFullList({
         sort: '-created',
       });
-      const filteredData = CategoryRecords.slice(0, 4);
-      setData(filteredData);
+      setData(CategoryRecords);
+      setFilteredData(CategoryRecords.slice(0, 4));
     }
     fetchCategory();
   }, []);
-  {
-    return (
-      <>
-        <section className="bg-dibCategoryBg text-center relative w-[1920px] h-[960px] m-auto flex flex-col">
-          <h2 className="text-dibBlack text-[32px] not-italic font-normal leading-[normal] tracking-[-1.5px] mt-[60px] mb-[20px]">
-            카테고리별 도서
-          </h2>
-          <strong className="text-dibBlack text-[20px] not-italic font-normal">
-            카테고리별 도서에서 원하는 키워드로 도서를 찾아보세요!
-          </strong>
-          <Link
-            to="/bookList"
-            className="absolute top-[190px] right-[410px]"
-            title="도서목록 더보기"
-          >
-            <FontAwesomeIcon icon={faPlus} className="w-[28px] h-[28px]" />
-          </Link>
-          <CategoryTabButton />
+
+  const handleSelectCategory = (selectedCategory) => {
+    const newFilteredData = data.filter((item) =>
+      item.category.includes(selectedCategory)
+    ).slice(0, 4); // 선택한 카테고리에 맞는 도서 중 최신순으로 4개만 필터링
+    setFilteredData(newFilteredData);
+  };
+
+  
+   return (
+     <>
+       <section className="bg-dibCategoryBg text-center relative w-[1920px] h-[960px] m-auto flex flex-col">
+         <h2 className="text-dibBlack text-[32px] not-italic font-normal leading-[normal] tracking-[-1.5px] mt-[60px] mb-[20px]">
+           카테고리별 도서
+         </h2>
+         <strong className="text-dibBlack text-[20px] not-italic font-normal">
+           카테고리별 도서에서 원하는 키워드로 도서를 찾아보세요!
+         </strong>
+         <Link
+           to="/bookList"
+           className="absolute top-[190px] right-[410px]"
+           title="도서목록 더보기"
+         >
+           <FontAwesomeIcon icon={faPlus} className="w-[28px] h-[28px]" />
+         </Link>
+         <CategoryTabButton htmlClick={() => handleSelectCategory("HTML")} cssClick={() => handleSelectCategory("CSS")} reactClick={() => handleSelectCategory("React")} javascriptClick={() => handleSelectCategory("JavaScript")} allClick={() => setFilteredData(data.slice(0,4))} />
 
           <div className="w-[1140px] my-10 mx-auto">
             <ul
               id="tab-panel-1"
               aria-labelledby="tab-1"
-              className="flex flex-wrap gap-x-5 gap-y-5"
+              className="flex flex-wrap gap-x-5 gap-y-5 justify-center"
             >
-              {data.map((item) => (
+              {filteredData.map((item) => (
                 <li key={item.id}>
                   <RowBookCard
                     imgSrc={item.book_image_link}
@@ -59,8 +69,7 @@ function CategoryBook() {
           </div>
         </section>
       </>
-    );
-  }
+   );
 }
 
 export default CategoryBook;
