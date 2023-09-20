08f76a1e64488ea820113510992aa50edca11ef1
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-20 12:42:17 +0900

    Feat : 신규 도서 Swiper 적용

--- src/components/main/NewBook.jsx
+++ src/components/main/NewBook.jsx
@@ -1,9 +1,15 @@
 import ScrollButton from '../common/ScrollButton';
-import { useEffect, useState } from 'react';
+import { useRef,useEffect, useState } from 'react';
 import { pb } from '@/api/pocketbase';
 import ColBookCard from '../common/bookCards/ColBookCard';
+import { Navigation, Scrollbar } from 'swiper/modules';
+import { Swiper } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
 
 function NewBook() {
+  const swiperRef = useRef(null);
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -28,19 +34,29 @@ function NewBook() {
             새롭게 소개하는 도서를 여기서 만나보세요!
           </strong>
           <div className="w-[1200px] mx-auto">
-            <ul className="flex justify-center gap-6 my-10">
-              {data.map((item) => (
-                <li key={item.id}>
-                  <ColBookCard
-                    imgSrc={item.book_image_link}
-                    imgAlt={item.book_title}
-                    nickName={item.user_id[0]}
-                    postTitle={item.post_title}
-                    bookTitle={item.book_title}
-                  />
-                </li>
-              ))}
-            </ul>
+            <div className="flex justify-center gap-6 my-10">
+              <Swiper
+                rewind={true}
+                navigation={true}
+                modules={[Navigation, Scrollbar]}
+                scrollbar={{
+                  hide: true,
+                }}
+                className="mySwiper"
+              >
+                {data.map((item) => (
+                  <li key={item.id}>
+                    <ColBookCard
+                      imgSrc={item.book_image_link}
+                      imgAlt={item.book_title}
+                      nickName={item.user_id[0]}
+                      postTitle={item.post_title}
+                      bookTitle={item.book_title}
+                    />
+                  </li>
+                ))}
+              </Swiper>
+            </div>
           </div>
           <div>
             <ScrollButton />
