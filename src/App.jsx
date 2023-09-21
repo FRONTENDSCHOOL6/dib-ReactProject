953216d2d828f489df3779ccf4834ad4d3e7ae94
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-21 22:08:59 +0900

    Feat : Context 적용

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Layout from './layout/Layout';
 import { Toaster } from 'react-hot-toast';
 import router from './routes/routes';
 import AuthProvider from './contexts/AuthContext';
+import PbDataProvider from './contexts/PbDataContext';
 
 function App() {
   return (
     <AuthProvider>
-      <RouterProvider router={router}>
-        <Layout />
-      </RouterProvider>
+      <PbDataProvider>
+        <RouterProvider router={router}>
+          <Layout />
+        </RouterProvider>
+      </PbDataProvider>
       <Toaster />
     </AuthProvider>
   );
---
+++ src/contexts/PbDataContext.jsx
@@ -0,0 +1,63 @@
+import { useEffect } from 'react';
+import { useState, createContext } from 'react';
+import { pb } from '@/api/pocketbase';
+import PropTypes from 'prop-types';
+import { useContext } from 'react';
+
+const PbDataContext = createContext();
+
+function PbDataProvider({ children }) {
+  const [bookData, setBookData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    pb.autoCancellation(false);
+    async function getPbData() {
+      try {
+        const allRecord = await pb.collection('posts').getFullList({
+          expand: 'user_id',
+        });
+        setBookData(allRecord);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        setIsLoading(false);
+      }
+    }
+    getPbData();
+  }, []);
+
+  const toggleLike = (postId) => {
+    setBookData((prevData) => {
+      return prevData.map((item) => {
+        if (item.id === postId) {
+          const newLikeCount = item.like_count + (item.isLiked ? -1 : 1);
+          return { ...item, like_count: newLikeCount, isLiked: !item.isLiked };
+        }
+        return item;
+      });
+    });
+  };
+
+  return (
+    <PbDataContext.Provider
+      value={{ setBookData, bookData, isLoading, toggleLike }}
+    >
+      {children}
+    </PbDataContext.Provider>
+  );
+}
+
+export default PbDataProvider;
+
+PbDataProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export function usePbData() {
+  const context = useContext(PbDataContext);
+  if (!context) {
+    throw new Error('usePbData must be used within a PbDataProvider');
+  }
+  return context;
+}
