b856e15c6d6379fed1728d9c63fb332dae4291ec
wlstmd1004v <wlstmd1004v@gmail.com>
2023-09-21 22:09:20 +0900

    Feat : pagination 구현

--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -1,84 +1,104 @@
-import { pb } from '@/api/pocketbase';
+import { useEffect, useState } from 'react';
 import Spinner from '@/components/bookList/Spinner';
 import TabButtonList from '@/components/bookList/TabButtonList';
 import ColBookCard from '@/components/common/bookCards/ColBookCard';
 import SubVisualBanner from '@/components/common/SubVisualBanner';
-import { useEffect, useState } from 'react';
+import { usePbData } from '@/contexts/PbDataContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 function BookList() {
-  const [data, setData] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("all");
-  const [isLoading, setIsLoading] = useState(false);
+  const { bookData, isLoading } = usePbData();
+  const perPage = 16;
+
+  const [categories] = useState(['all', 'HTML', 'CSS', 'JavaScript', 'React']);
+  const [selectedCategory, setSelectedCategory] = useState('all');
+  const [categoryData, setCategoryData] = useState({});
+  const [currentPage, setCurrentPage] = useState(1);
+
   useEffect(() => {
-    pb.autoCancellation(false);
-    async function fetchBooksList() {
-      setIsLoading(true); 
-      try {
-        const allRecord = await pb.collection('posts').getFullList({
-          expand : 'user_id',
-        });
-        console.log(allRecord);
-        setData(allRecord);
-        setFilteredData(allRecord);
-      } catch (error) {
-        console.error(error);
-      }
-      setIsLoading(false);
+    // 카테고리별 데이터 초기화
+    const dataByCategory = {};
+    for (const category of categories) {
+      dataByCategory[category] = bookData.filter((item) =>
+        category === 'all' ? true : item.category.includes(category)
+      );
     }
-    fetchBooksList();
-  }, []);
+    setCategoryData(dataByCategory);
+    setSelectedCategory('all');
+  }, [bookData, categories]);
 
-  const handleSelectCategory = (e, selectedCategory) => {
-    e.preventDefault();
-    const newFilteredData = data.filter((item) =>
-      item.category.includes(selectedCategory)
+  const handleSelectCategory = (selectedCategory) => {
+    setSelectedCategory(selectedCategory);
+    setCurrentPage(1); // 카테고리 변경 시 페이지 초기화
+  };
+
+  const handlePageChange = (newPage) => {
+    const totalPages = Math.ceil(
+      categoryData[selectedCategory].length / perPage
     );
-    setFilteredData(newFilteredData);
+    if (newPage >= 1 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+    }
   };
-
-  const handleHTMLCategory = (e) => {
-    handleSelectCategory(e, "HTML");
-    setSelectedCategory("HTML");
-  };
-
-  const handleCSSCategory = (e) => {
-    handleSelectCategory(e, "CSS");
-    setSelectedCategory("CSS");
-  };
-
-  const handleReactCategory = (e) => {
-    handleSelectCategory(e, "React");
-    setSelectedCategory("React");
-  };
-
-  const handleJsCategory = (e) => {
-    handleSelectCategory(e, "JavaScript");
-    setSelectedCategory("JavaScript");
-  };
-
-  
 
   return (
     <>
       <SubVisualBanner title="도서목록" />
-      <TabButtonList selected={selectedCategory} htmlClick={handleHTMLCategory} cssClick={handleCSSCategory} reactClick={handleReactCategory} javascriptClick={handleJsCategory} allClick={() => {setFilteredData(data);setSelectedCategory("all");}} />
+      <TabButtonList
+        selected={selectedCategory}
+        htmlClick={() => handleSelectCategory('HTML')}
+        cssClick={() => handleSelectCategory('CSS')}
+        reactClick={() => handleSelectCategory('React')}
+        javascriptClick={() => handleSelectCategory('JavaScript')}
+        allClick={() => handleSelectCategory('all')}
+      />
 
       <div className="w-[1920px] m-auto">
-      {isLoading ? (
-          <Spinner/>
-      ) : (
-        <ul
-          id="tab-panel-1"
-          aria-labelledby="tab-1"
-          className="w-[1200px] m-auto flex flex-wrap gap-x-6 gap-y-10 justify-start mt-16 mb-20"
-        >
-          {filteredData.map((item) => (
-            <li key={item.id}>
-              <ColBookCard imgSrc={item.book_image_link} imgAlt={item.book_title} nickName={item.expand.user_id[0].nickname} postTitle={item.post_title}bookTitle={item.book_title}/>
-            </li>
-          ))}
-        </ul>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <>
+            <ul
+              id="tab-panel-1"
+              aria-labelledby="tab-1"
+              className="w-[1200px] m-auto flex flex-wrap gap-x-6 gap-y-10 justify-start mt-16 mb-20"
+            >
+              {categoryData[selectedCategory]
+                .slice(
+                  (currentPage - 1) * perPage,
+                  currentPage * perPage
+                )
+                .map((item) => (
+                  <li key={item.id}>
+                    <ColBookCard
+                      imgSrc={item.book_image_link}
+                      imgAlt={item.book_title}
+                      nickName={item.expand.user_id[0].nickname}
+                      postTitle={item.post_title}
+                      bookTitle={item.book_title}
+                    />
+                  </li>
+                ))}
+            </ul>
+            <div className="flex justify-center mt-4">
+              <button
+                onClick={() => handlePageChange(currentPage - 1)}
+                disabled={currentPage === 1}
+              >
+                <FontAwesomeIcon icon={faAngleLeft} />
+              </button>
+              <span className="mx-2">
+                {currentPage} / {Math.ceil(categoryData[selectedCategory].length / perPage)}
+              </span>
+              <button
+                onClick={() => handlePageChange(currentPage + 1)}
+                disabled={currentPage >= Math.ceil(categoryData[selectedCategory].length / perPage)}
+              >
+                <FontAwesomeIcon icon={faAngleRight} />
+              </button>
+            </div>
+          </>
         )}
       </div>
     </>
