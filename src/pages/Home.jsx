081d06e5eefa51b490bc7c88707aae19bda84259
yena <spffhd0835@gmail.com>
2023-09-22 11:42:29 +0900

    Fix : 메인페이지 로딩시 전체 스피너 적용제거 각 섹션에 적용

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,18 @@
-import { useState, useEffect } from 'react';
 import MainBanner from '@/components/main/MainBanner';
 import BestBook from '@/components/main/BestBook';
 import CategoryBook from '@/components/main/CategoryBook';
 import NewBook from '@/components/main/NewBook';
-import Spinner from '@/components/bookList/Spinner';
+import { useState } from 'react';
 
 function Home() {
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-  }, []);
-
   return (
     <>
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <>
-          <MainBanner />
-          <BestBook />
-          <CategoryBook />
-          <NewBook />
-        </>
-      )}
+      <MainBanner />
+      <BestBook isLoading={isLoading} setIsLoading={setIsLoading}/>
+      <CategoryBook isLoading={isLoading} setIsLoading={setIsLoading}/>
+      <NewBook />
     </>
   );
 }
