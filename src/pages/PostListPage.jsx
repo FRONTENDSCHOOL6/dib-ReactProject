e8f56e21cf298dcc58a058e0e466cfcc1154a72a
jjang-aaa <jadoit473@gmail.com>
2023-09-22 11:39:34 +0900

    Feat: 게시글 목록 페이지 리스트 컴포넌트 빼고 기능구현, 버튼요소 수정

--- src/pages/PostListPage.jsx
+++ src/pages/PostListPage.jsx
@@ -1,12 +1,67 @@
-import PostList from '@/components/postList/PostList';
+import { pb } from '@/api/pocketbase';
+import Spinner from '@/components/bookList/Spinner';
+import ColBookCard from '@/components/common/bookCards/ColBookCard';
 import PostListTitle from '@/components/postList/PostListTitle';
 import PostWriteButton from '@/components/postList/PostWriteButton';
+import { useAuth } from '@/contexts/AuthContext';
+import { useEffect, useState } from 'react';
 
 function PostListPage() {
+  const { user } = useAuth();
+  const [writtenData, setWrittenData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function getUserData() {
+      setIsLoading(true);
+      if (user) {
+        const userIdData = await pb.collection('users').getOne(user.id, {
+          expand: 'bookmark_posts',
+        });
+
+        const writtenPosts = userIdData.written_posts;
+
+        const reviewData = await pb.collection('posts').getFullList({
+          expand: 'user_id',
+        });
+
+        const myWrittenReviews = reviewData.filter((review) =>
+          writtenPosts.includes(review.id)
+        );
+        setWrittenData(myWrittenReviews);
+      }
+      setIsLoading(false);
+    }
+    getUserData();
+  }, [user]);
+
   return (
     <div className="mb-[100px]">
       <PostListTitle />
-      <PostList />
+      <div>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <ul
+            id="tab-panel-1"
+            aria-labelledby="tab-1"
+            className="w-[1200px] m-auto flex flex-wrap gap-x-6 gap-y-10 justify-start mt-16 mb-20"
+          >
+            {writtenData &&
+              writtenData.map((item) => (
+                <li key={item.id}>
+                  <ColBookCard
+                    imgSrc={item.book_image_link}
+                    imgAlt={item.book_title}
+                    nickName={item.expand.user_id[0].nickname}
+                    postTitle={item.post_title}
+                    bookTitle={item.book_title}
+                  />
+                </li>
+              ))}
+          </ul>
+        )}
+      </div>
       <PostWriteButton />
     </div>
   );
