6d2b137f5265b67fa89ed294725623667d2472b8
jjang-aaa <jadoit473@gmail.com>
2023-09-24 04:30:25 +0900

    Chore: 잘못건드린 파일 원상복구

--- src/components/userPost/PostTitle.jsx
+++ src/components/userPost/PostTitle.jsx
@@ -1,13 +1,17 @@
-import ProfileView from '@/layout/ProfileView';
 import PropTypes from 'prop-types';
 
-function PostTitle({ postTitle, cearteDate }) {
+function PostTitle({ postTitle, userName, cearteDate, userImg }) {
   return (
     <div className="flex items-start flex-col m-auto mb-[60px] w-[1200px] border-b border-horizontal">
       <h3 className="mb-10 text-[28px]">{postTitle}</h3>
       <div className="flex items-center justify-between mb-7 w-[1200px]">
         <div className="flex justify-center items-center">
-          <ProfileView />
+          <img
+            src={userImg}
+            alt="유저 아이콘 이미지"
+            className="rounded-[50%] w-[30px] h-[30px] mr-[8px]"
+          />
+          <span className="text-base mr-[25px]">{userName}</span>
           <span className="text-horizontal">{cearteDate}</span>
         </div>
         <div className="flex items-center">
