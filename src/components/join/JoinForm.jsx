35c0a5c81766087558be0c484969b65175e7c0d6
jjang-aaa <jadoit473@gmail.com>
2023-09-23 22:29:17 +0900

    Feat: 데이터 통신 후 값을 받아와서 유저의 id를 프로필페이지에 전달하는 코드 추가

--- src/components/join/JoinForm.jsx
+++ src/components/join/JoinForm.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react"
-import Button from "../common/Button"
-import FormInput from "../common/FormInput"
-import InputValidation from "../common/InputValidation"
-import debounce from "@/utils/debounce";
-import { emailReg, nameReg, pwReg } from "@/utils/regular";
-import { pb } from "@/api/pocketbase";
-import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import Button from '../common/Button';
+import FormInput from '../common/FormInput';
+import InputValidation from '../common/InputValidation';
+import debounce from '@/utils/debounce';
+import { emailReg, nameReg, pwReg } from '@/utils/regular';
+import { pb } from '@/api/pocketbase';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 function JoinForm() {
   const navigate = useNavigate();
@@ -27,36 +27,35 @@ function JoinForm() {
   let passwordError = '';
   let confirmPwdError = '';
   let samePwd = false;
-  
-  if(!formState.name){
+
+  if (!formState.name) {
     nameError = '';
-  }else if(!nameCheck){
+  } else if (!nameCheck) {
     nameError = '한글을 사용해 입력해주세요. (특수기호, 공백 사용 불가)';
   }
 
-  if(!formState.email){
+  if (!formState.email) {
     emailError = '';
-  }else if(!emailCheck){
+  } else if (!emailCheck) {
     emailError = '올바른 이메일 형식을 입력해주세요';
   }
 
-  if(!formState.password){
+  if (!formState.password) {
     passwordError = '';
-  }else if(!pwdCheck){
+  } else if (!pwdCheck) {
     passwordError = '8~20자의 영문, 숫자, 특수문자를 포함해주세요.';
   }
 
-  if(!formState.passwordConfirm){
+  if (!formState.passwordConfirm) {
     confirmPwdError = '';
-  }else if(formState.password !== formState.passwordConfirm){
+  } else if (formState.password !== formState.passwordConfirm) {
     confirmPwdError = '비밀번호가 일치하지 않습니다.';
     samePwd = false;
-  }else{
+  } else {
     samePwd = true;
   }
 
-
-  const handleRegister = async (e) =>{
+  const handleRegister = async (e) => {
     e.preventDefault();
 
     const userData = {
@@ -65,9 +64,10 @@ function JoinForm() {
       emailVisibility: true,
     };
 
-    await pb.collection('users').create(userData);
+    const joinData = await pb.collection('users').create(userData);
+    console.log(joinData.id);
 
-    toast.success('프로필 등록 후 회원가입이 완료됩니다.',{
+    toast.success('프로필 등록 후 회원가입이 완료됩니다.', {
       position: 'top-center',
       duration: 3000,
       icon: '💌',
@@ -77,9 +77,9 @@ function JoinForm() {
       },
     });
     setTimeout(() => {
-      navigate('/registerProfile');
+      navigate('/registerProfile', { state: { userId: joinData.id } });
     }, 1000);
-  }
+  };
 
   const handleInput = (e) => {
     const { name, value } = e.target;
@@ -90,7 +90,7 @@ function JoinForm() {
   };
 
   const handleDebounceInput = debounce(handleInput, 500);
-  
+
   const handleTogglePasswordVisibility = () => {
     setIsPasswordVisible(!isPasswordVisible);
   };
@@ -103,68 +103,78 @@ function JoinForm() {
 
   return (
     <form
-        onSubmit={handleRegister}
-        method="post"
-        className="flex flex-col gap-5 w-[500px] m-auto mt-10"
-      >
-        <div className="flex flex-col gap-1 text-left text-base">
-          <FormInput type="text" name="name" id="name" placeholder="본인 이름을 입력해주세요" label="실명"
+      onSubmit={handleRegister}
+      method="post"
+      className="flex flex-col gap-5 w-[500px] m-auto mt-10"
+    >
+      <div className="flex flex-col gap-1 text-left text-base">
+        <FormInput
+          type="text"
+          name="name"
+          id="name"
+          placeholder="본인 이름을 입력해주세요"
+          label="실명"
           defaultValue={formState.name}
           onChange={handleDebounceInput}
-          />
-          <InputValidation message={nameError}/>
-        </div>
-
-        <div className="flex flex-col gap-1 text-left text-base">
-          <FormInput type="email" name="email" id="email" placeholder="예) email@address.com" label="이메일"
-            defaultValue={formState.email}
-            onChange={handleDebounceInput}
-          />
-          <InputValidation message={emailError}/>
-        </div>
-
-        <div className="flex flex-col gap-1 text-left text-base relative">
-          <FormInput
-            type={isPasswordVisible ? "text" : "password"}
-            name="password"
-            id="password"
-            placeholder="비밀번호(영문/숫자/특수문자 조합 8~20자)"
-            label="비밀번호"
-            defaultValue={formState.password}
-            onChange={handleDebounceInput}
-          />
-          <img 
-            src={isPasswordVisible ? "/view.png" : "/hide.png"} 
-            alt={isPasswordVisible ? "숨김" : "보임"}
-            role='button' 
-            className="w-[25px absolute right-5 top-11 cursor-pointer"
-            onClick={handleTogglePasswordVisibility}
-            tabIndex={0}
-            onKeyDown={handleKeyDown}
-          />
-          <InputValidation message={passwordError}/>
-        </div>
-
-        <div className="flex flex-col gap-1 text-left text-base mb-10">
-          <FormInput 
-            type="password" 
-            name="passwordConfirm" 
-            id="passwordConfirm" 
-            placeholder="비밀번호를 다시 입력해주세요" 
-            label="비밀번호 확인"
-            defaultValue={formState.passwordConfirm}
-            onChange={handleDebounceInput}
-          />
-          <InputValidation message={confirmPwdError}/>
-        </div>
-
-        <Button 
-          type="submit" 
-          title="회원가입" 
-          disabled = {!(samePwd && emailCheck && pwdCheck && nameCheck)}
         />
-      </form>
-  )
+        <InputValidation message={nameError} />
+      </div>
+
+      <div className="flex flex-col gap-1 text-left text-base">
+        <FormInput
+          type="email"
+          name="email"
+          id="email"
+          placeholder="예) email@address.com"
+          label="이메일"
+          defaultValue={formState.email}
+          onChange={handleDebounceInput}
+        />
+        <InputValidation message={emailError} />
+      </div>
+
+      <div className="flex flex-col gap-1 text-left text-base relative">
+        <FormInput
+          type={isPasswordVisible ? 'text' : 'password'}
+          name="password"
+          id="password"
+          placeholder="비밀번호(영문/숫자/특수문자 조합 8~20자)"
+          label="비밀번호"
+          defaultValue={formState.password}
+          onChange={handleDebounceInput}
+        />
+        <img
+          src={isPasswordVisible ? '/view.png' : '/hide.png'}
+          alt={isPasswordVisible ? '숨김' : '보임'}
+          role="button"
+          className="w-[25px absolute right-5 top-11 cursor-pointer"
+          onClick={handleTogglePasswordVisibility}
+          tabIndex={0}
+          onKeyDown={handleKeyDown}
+        />
+        <InputValidation message={passwordError} />
+      </div>
+
+      <div className="flex flex-col gap-1 text-left text-base mb-10">
+        <FormInput
+          type="password"
+          name="passwordConfirm"
+          id="passwordConfirm"
+          placeholder="비밀번호를 다시 입력해주세요"
+          label="비밀번호 확인"
+          defaultValue={formState.passwordConfirm}
+          onChange={handleDebounceInput}
+        />
+        <InputValidation message={confirmPwdError} />
+      </div>
+
+      <Button
+        type="submit"
+        title="회원가입"
+        disabled={!(samePwd && emailCheck && pwdCheck && nameCheck)}
+      />
+    </form>
+  );
 }
 
-export default JoinForm
+export default JoinForm;
