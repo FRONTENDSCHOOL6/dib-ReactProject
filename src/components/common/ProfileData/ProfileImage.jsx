53c8959b92089cd4140f068b10d3e368888d0110
jjang-aaa <jadoit473@gmail.com>
2023-09-25 02:48:39 +0900

    Feat: 게시글, 댓글에 프로필 이미지 추가

--- src/components/common/ProfileData/ProfileImage.jsx
+++ src/components/common/ProfileData/ProfileImage.jsx
@@ -4,9 +4,13 @@ import PropTypes from 'prop-types';
 function ProfileImage({ width = 'w-[25px]', height = 'h-[25px]' }) {
   const { user } = useAuth();
 
+  const imageUrl = user
+    ? `https://db-dib.pockethost.io/api/files/_pb_users_auth_/${user.id}/${user.profileImage}`
+    : '/defaultProfile.png';
+
   return (
     <img
-      src={`https://db-dib.pockethost.io/api/files/_pb_users_auth_/${user.id}/${user.profileImage}`}
+      src={imageUrl}
       alt="프로필 이미지"
       className={`${width} ${height} rounded-full`}
     />
