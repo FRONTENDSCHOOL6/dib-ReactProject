a51e2f37844fbd70fabed5aca2ada8e9a524a5f9
yena <spffhd0835@gmail.com>
2023-09-24 18:16:14 +0900

    Fix : AuthContext 오류 수정

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,53 +1,32 @@
 import { createContext, useEffect, useState, useContext } from 'react';
 import { string, node } from 'prop-types';
 import { pb } from '@/api/pocketbase';
-import useStorage from '@/hooks/useStorage';
 
-// createContext얘로 AuthContext 상태를 담아주는 아이를 만듦
 const AuthContext = createContext();
 
-// 스테이트의 초깃값
+
 const initialAuthState = {
   isAuth: false,
   user: null,
   token: '',
 };
-// Context.Provider 래퍼 컴포넌트 작성
-function AuthProvider({ displayName = 'AuthProvider', children }) {
-  // 로컬스토리지에 있는 데이터를 가져옴
-  const { storageData, setStorageData } = useStorage('pocketbase_auth');
 
-  // 인증 상태를 담아두는 스테이트
+function AuthProvider({ displayName = 'AuthProvider', children }) {
   const [authState, setAuthState] = useState(initialAuthState);
 
-  // 로컬스토리지의 값이 있으면 넣어주고 없으면 안넣어줌
   useEffect(() => {
-    if (storageData) {
-      const { token, model } = storageData;
+    if (pb.authStore.isValid) {
+      const { token, model } = pb.authStore;
       setAuthState({
         isAuth: !!model,
         user: model,
         token,
       });
-    } else {
-      // 로컬스토리지에 값이 없으면 데이터베이스에서 가져와서 저장
-      pb.authStore.get().then(({ token, model }) => {
-        if (model) {
-          setStorageData({ token, model });
-          setAuthState({
-            isAuth: true,
-            user: model,
-            token,
-          });
-        }
-      });
     }
-  }, [storageData]);
+  }, []);
 
   useEffect(() => {
-    // 이건 자세히 모르겠지만 처음에 랜더링 될때 한번 실행되는데 상태값이 변하면 다시 넣어주는 아이인듯
     const unsub = pb.authStore.onChange((token, model) => {
-      setStorageData({ token, model });
       setAuthState((state) => ({
         ...state,
         isAuth: !!model,
@@ -76,7 +55,6 @@ function AuthProvider({ displayName = 'AuthProvider', children }) {
     return await pb.authStore.clear();
   };
 
-  // 가져온 값을 전체 prop에 넣어서 뿌려준다
   const authValue = {
     ...authState,
     join,
@@ -84,7 +62,6 @@ function AuthProvider({ displayName = 'AuthProvider', children }) {
     logOut,
   };
 
-  // children이 이 값을 뿌려줄 페이지, 근데 전체에 로그인 넣을 꺼니까 app에 넣자
   return (
     <AuthContext.Provider value={authValue} displayName={displayName}>
       {children}
@@ -96,15 +73,12 @@ AuthProvider.propTypes = {
   displayName: string,
   children: node.isRequired, // React.ReactNode
 };
-
 export default AuthProvider;
 
-// 인증 정보를 앱 어디서나 받아 쓸 수 있도록 하는 함수
 export const useAuth = () => {
   const authValue = useContext(AuthContext);
   if (!authValue) {
     throw new Error('useAuth 훅은 AuthProvider 내부에서만 사용할 수 있습니다.');
   }
-
   return authValue;
 };
